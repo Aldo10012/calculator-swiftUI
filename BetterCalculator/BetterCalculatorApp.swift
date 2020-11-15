c6b0e7caab744b485a11918350ce3628facaaf8c
Alberto <aldo10012@gmail.com>
2020-11-15 09:25:54 -0800

    Initial Commit

---
+++ BetterCalculator/BetterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCalculatorApp.swift
+//  BetterCalculator
+//
+//  Created by Alberto Dominguez on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
